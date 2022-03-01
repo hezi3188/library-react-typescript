a17694c6f46cbaf2f3d9b874af9086143c62cdf8
hezi3188 <hezi3188@gmail.com>
2022-03-01 10:12:23 +0200

    add redux ts

--- src/components/app/readers/readersMangement.tsx
+++ src/components/app/readers/readersMangement.tsx
@@ -4,8 +4,12 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 import { Reader } from '../../../models/reader';
 import { LOAD_READERS } from '../../../GraphQL/Queries';
-
+import { RootState } from '../../../redux/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../../../redux/counter';
 const ReadersMangement: React.FC = () => {
+  const count = useSelector<RootState, number>((state) => state.counter.value);
+  const dispatch = useDispatch();
   const { error, loading, data } = useQuery(LOAD_READERS);
   const [readers, setReaders] = useState<Reader[]>([]);
   useEffect(() => {
@@ -21,7 +25,23 @@ const ReadersMangement: React.FC = () => {
   }
   return (
     <div>
-      ff
+      <div>
+        <div>
+          <button
+            aria-label='Increment value'
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label='Decrement value'
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
       {readers.map((val: Reader) => {
         return <h1> {val.firstName}</h1>;
       })}
