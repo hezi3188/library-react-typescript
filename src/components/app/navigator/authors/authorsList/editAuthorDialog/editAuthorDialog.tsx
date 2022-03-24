4e1769f04bec73e29d57d7e7c163a75cc1164dfb
hezi3188 <hezi3188@gmail.com>
2022-03-24 15:01:22 +0200

    finish all project without subscriptions

--- src/components/app/navigator/authors/authorsList/editAuthorDialog/editAuthorDialog.tsx
+++ src/components/app/navigator/authors/authorsList/editAuthorDialog/editAuthorDialog.tsx
@@ -7,9 +7,12 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import { useStyles } from './editAuthorDialogStyles';
-import useAddBookDialog from './useEditAuthorDialog';
+import useEditAuthorDialog from './useEditAuthorDialog';
 import { Author } from '../../../../../../models/author';
 
 const EDIT_AUTHOR_TITLE: string = 'פרטי משתמש';
@@ -17,61 +20,75 @@ const EDIT_AUTHOR_BUTTON: string = 'שמור שינויים';
 const FIRST_NAME: string = 'שם פרטי';
 const LAST_NAME: string = 'שם משפחה';
 
+const schema = yup.object().shape({
+  firstName: yup.string().required('שם פרטי הוא שדה חובה'),
+  lastName: yup.string().required('שם משפחה הוא שדה חובה'),
+});
+
 interface Props {
   open: boolean;
   onClose: () => void;
   author?: Author;
 }
-const AddBookDialog: React.FC<Props> = (props) => {
+const EditAuthorDialog: React.FC<Props> = (props) => {
   const { open, onClose, author } = props;
   const classes = useStyles();
+
+  const { register, reset, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const handleClose = () => {
     onClose();
   };
   const [firstName, setFirstName] = useState<string>();
   const [lastName, setLastName] = useState<string>();
-  const { editAuthor: addBook } = useAddBookDialog({
+  const { editAuthor: addBook } = useEditAuthorDialog({
     handleClose,
   });
 
   useEffect(() => {
+    reset();
     setFirstName(author?.firstName);
     setLastName(author?.lastName);
-  }, [author, onClose]);
+  }, [author, onClose, reset]);
 
+  const submitForm = () => {
+    addBook(firstName as string, lastName as string, author?.id as number);
+  };
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>{EDIT_AUTHOR_TITLE}</DialogTitle>
       <DialogContent>
-        <FormControl className={classes.select}>
+        <FormControl
+          onSubmit={handleSubmit(submitForm)}
+          className={classes.select}
+        >
           <TextField
             label={FIRST_NAME}
             variant='outlined'
             required
+            error={formState.errors.firstName}
             defaultValue={firstName}
+            inputProps={{ ...register('firstName') }}
+            helperText={formState.errors.firstName?.message}
             onChange={(e) => setFirstName(e.target.value)}
           />
+
           <TextField
             required
+            error={formState.errors.lastName}
+            helperText={formState.errors.lastName?.message}
             label={LAST_NAME}
             variant='outlined'
             defaultValue={lastName}
+            inputProps={{ ...register('lastName') }}
             onChange={(e) => setLastName(e.target.value)}
           />
         </FormControl>
       </DialogContent>
       <DialogActions>
-        <Button
-          disabled={!firstName || !lastName}
-          onClick={() =>
-            addBook(
-              firstName as string,
-              lastName as string,
-              author?.id as number
-            )
-          }
-        >
+        <Button type='submit' id='submit' onClick={handleSubmit(submitForm)}>
           {EDIT_AUTHOR_BUTTON}
         </Button>
       </DialogActions>
@@ -79,4 +96,4 @@ const AddBookDialog: React.FC<Props> = (props) => {
   );
 };
 
-export default AddBookDialog;
+export default EditAuthorDialog;
